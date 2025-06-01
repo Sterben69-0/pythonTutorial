cf98de074492d8e063fadfbf997816d2f03ac702
Sterben69-0 <moeedjutt001@gmail.com>
2025-06-02 02:14:02 +0800

    strings

---
+++ basics/conditional_operator.py
@@ -0,0 +1,37 @@
+# Using conditional operator
+# ? Using if, elif and else statement
+
+# sal = int(input("What is your Salary?"))
+# if sal == 50000:
+#     print("tax=", sal * 0.1)
+# elif sal > 50000:
+#     print("tax=", sal * 0.2)
+# else:
+#     print("You are poor")
+
+# #! Nesting
+
+# age = int(input("How old are you?"))
+
+# if age >= 18:
+#     if age >= 80:
+#         print("You are too old to drive")
+#     else:
+#         print("Can drive")
+# else:
+#     print("Too young to drive")
+
+# * Using conditional operator and logical operator
+
+num1 = int(input("enter a number"))
+num2 = int(input("enter one more"))
+num3 = int(input("one last"))
+num4 = int(input("ok this is the last one"))
+if num1 > num2 and num1 > num3 and num1 > num4:
+    print(num1)
+elif num2 > num3 and num2 > num4:
+    print(num2)
+elif num3 > num4:
+    print(num3)
+else:
+    print(num4)
---
+++ basics/string.py
@@ -0,0 +1,17 @@
+## Strings
+# Strings are immutable sequences of characters enclosed in quotes.
+str1 = "Hello, World!"
+print(str1)  # Output: Hello, World!
+# We can alse use single quotes
+str2 = "Hello, World!"
+# We can also use triple quotes for multi-line strings
+str3 = """This is a multi-line string.
+It can span multiple lines."""
+print(str3)  # Output: This is a multi-line string. It can span multiple lines.
+
+
+str = "learing python"
+len_str = len(str)
+print("Length of the string:", len_str)
+str = "apple"
+print(str[-3:-1])  # Slicing the string
